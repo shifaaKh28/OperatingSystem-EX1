6ccc54f361d369a5da8fb74dfe6dbca3a96b914d
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2024-05-14 22:14:45 +0300

    Update main.c

--- Q3/main.c
+++ Q3/main.c
@@ -1,15 +1,14 @@
 #include "poisson.h"
 
-#define VALUES 5
-
 int main(int argc, char *argv[]){
 
-    int k[] = {1, 10, 2, 3, 3};
-    int lambda[] = {2, 2, 2, 3, 100};
+    int k[] = {1, 10, 2, 3, 3};//array for the k values
+    int lambda[] = {2, 2, 2, 3, 100};// array for the lambda
 
-    for (int i = 0; i < VALUES; i++)
+    for (int i = 0; i < 5; i++)
     {
-        printf("Run #%d: lambda = %d, k = %d, poisson_calc = %Lf\n", (i + 1), 
+        //call the poisson_calc
+        printf("%d): lambda = %d, k = %d, poisson_calc = %Lf\n", (i + 1), 
         lambda[i], k[i], poisson_calc(lambda[i], k[i]));
     }
     
